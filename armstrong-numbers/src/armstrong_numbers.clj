00190949db4faf95f1c01929d2db76c9cb5f03db
Pedro Ramos <ramuspedro@gmail.com>
2019-08-31 12:00:19 -0300

    added: two fer test

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -7,13 +7,10 @@
     (if (number? n) n nil)))
 
 (defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-  (def num-str (str num))
-  (def num-size (count num-str)) ; size of num string
-  (def num-list (str/split num-str #"")) ; convert the number string to a list
-  (def num-vector (map String->Number num-list)) ; convert all the strings to number
 
-  (= num (reduce + (for [i num-vector] (math/expt i num-size))))
+  (= num (reduce + (for 
+                      [i (map String->Number (str/split (str num) #""))] 
+                         (math/expt i (count (str num))))))
 )
 
 ; (str/split "1234" #"")
